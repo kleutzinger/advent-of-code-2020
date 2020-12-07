e300065b57619c979c1022e7e5e70a7ae904871f
kleutzinger <6435727+kleutzinger@users.noreply.github.com>
2020-12-07 10:34:56 -0800

    add shortcuts

--- run.py
+++ run.py
@@ -24,4 +24,7 @@ def line_transform(line):
 line_groups = data.split("\n\n")  # lines split by double newlines
 lines = [line_transform(line) for line in lines]  # apply line_transform to each line
 
+L, I, D = list, int, dict
+P, M = print, map
+S, J = str.split, str.join
 ## end of boilerplate
