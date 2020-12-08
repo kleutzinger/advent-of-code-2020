0b8b4668520e3bebcb170f92c15f82100d388cbc
kleutzinger <6435727+kleutzinger@users.noreply.github.com>
2020-12-07 20:50:26 -0800

    rename boilerplate

--- new_day.py
+++ new_day.py
@@ -5,5 +5,5 @@ import os
 dt = datetime.datetime.today()
 folder_num = str(dt.day + 1).zfill(2)
 os.system("mkdir " + folder_num)
-os.system("cp run.py " + folder_num)
+os.system("cp boilerplate.py " + os.path.join(folder_num, "run.py"))
 os.system("touch " + os.path.join(folder_num, "input.txt"))
