a0c4bc1fbceaca2dcb84f0fa79fe9d33ebb1b2a9
kleutzinger <6435727+kleutzinger@users.noreply.github.com>
2020-12-10 13:34:54 -0800

    finish up day 4

--- 04/run.py
+++ 04/run.py
@@ -1,11 +1,11 @@
 lines = []
 with open("input.txt") as f:
-    lines = [x.strip().split(" ") for x in f.readlines()]
+    # lines = [x.strip() for x in f.readlines()]
+    lines = f.read()
 print(lines)
 print(len(lines), "lines in input.txt")
 # split = [line.split() for line in lines]
 # print(split)
-
 """
 byr (Birth Year)
 iyr (Issue Year)
@@ -16,16 +16,107 @@ ecl (Eye Color)
 pid (Passport ID)
 cid (Country ID)
 """
-required = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
-ports = [[]]
-seen = dict()
-for l in lines:
-    things = [l.split(":") for l]
-    for thing in things:
-        seen[thing] = True
-    if l == "\n":
-        seen = dict()
-    if all([i in required for i in seen.keys()]):
-        print("true")
 
-print(ports)
+
+passports = lines.split("\n\n")
+passports = [p.replace("\n", " ").strip() for p in passports]
+from pprint import pprint
+
+
+required = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"]
+
+valid = 0
+for p in passports:
+    if all([r in p for r in required]):
+        valid += 1
+
+print(valid)
+
+## 2
+"""
+byr (Birth Year) - four digits; at least 1920 and at most 2002.
+iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+hgt (Height) - a number followed by either cm or in:
+If cm, the number must be at least 150 and at most 193.
+If in, the number must be at least 59 and at most 76.
+hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+pid (Passport ID) - a nine-digit number, including leading zeroes.
+cid(Country ID) - ignored, missing or not.
+"""
+
+import re
+
+
+def valid_height(s):
+    try:
+        pass
+        unit = s[-2:]
+        num = int(s[:-2])
+        if unit not in ["in", "cm"]:
+            return False
+        if unit == "cm":
+            return num >= 150 and num <= 193
+        else:
+            return num >= 59 and num <= 76
+    except:
+        return False
+
+
+prog = re.compile(r"^#[0-9a-f]{6}$")
+pid = re.compile(r"^[0-9]{9}$")
+req_validators = [
+    lambda x: int(x) >= 1920 and int(x) <= 2002,
+    lambda x: int(x) >= 2010 and int(x) <= 2020,
+    lambda x: int(x) >= 2020 and int(x) <= 2030,
+    valid_height,
+    lambda x: bool(prog.match(x)),
+    lambda x: x in "amb blu brn gry grn hzl oth".split(" "),
+    lambda x: bool(pid.match(x)),
+]
+
+
+def get_val(p, key):
+    for thing in p.split(" "):
+        if not ":" in thing:
+            continue
+        k, v = thing.split(":")
+        if k == key:
+            return v
+
+
+# def get_value(p, f):
+#     for i, col_split in enumerate(p.split(":")):
+#         print(col_split)
+#         if col_split[0] == f:
+#             return p.split(":")[i + 1].split(" ")[0]
+#     return ""
+
+
+def find_and_validate_all(p):
+    for idx in range(7):
+        try:
+            key = required[idx]
+            validator = req_validators[idx]
+            val = get_val(p, key)
+            if val == None:
+                return False
+            print(val)
+            if not validator(get_val(p, key)):
+                print("bad")
+                return False
+        except Exception as err:
+            print(err)
+            print("except")
+            return False
+    print("good")
+    return True
+
+
+valid = 0
+for p in passports:
+    if find_and_validate_all(p):
+        valid += 1
+
+print("final answer: ", valid)  ## 160
