0ecf98180df4f52ace1affce67c49b71cb985535
kleutzinger <6435727+kleutzinger@users.noreply.github.com>
2020-12-13 01:38:28 -0800

    well, i got it

--- 13/run.py
+++ 13/run.py
@@ -4,7 +4,8 @@ import os
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 try:
-    with open("input_small.txt") as f:
+    with open("input.txt") as f:
+        # with open("input_small.txt") as f:
         data = f.read()  # entire file as string
         lines = data.splitlines()
 except:
@@ -58,26 +59,37 @@ for d in lines[1].split(","):
         waits.append(m)
 ans(min_ans[-1])  # 2995
 
+## part 2 ##
+
 departs = []
 offsets = []
-for i, d in E(lines[1].split(",")):
+offsets2 = []
+offsets3 = []
+for i, d in enumerate(lines[1].split(",")):
     if d != "x":
         d = int(d)
         departs.append(d)
         offsets.append(i)
+        while i > d:
+            i -= d
+        offsets2.append(i)
+        offsets3.append(d - i)
         print(i)
 
 
 first = departs[0]
 idx = 0
 
+from math import gcd  # Python versions 3.5 and above
+
 # depart mod clocktime == offset
 def allmod(m, l):
-
     return list(map(lambda e: m % e, l))
 
 
-def correct_time(time, L):
+def correct_time(time, L=departs):
+    # show buses at correct time + offset
+    # return [True, False, False, ...]
     corrects = []
     Z = zip(departs, offsets)
     for dep, goal_minute in Z:
@@ -91,6 +103,8 @@ def correct_time(time, L):
 
 
 def quick_correct(time):
+    # quickly show if time is correct
+    # return True/False
     Z = zip(departs, offsets)
     for dep, goal_minute in Z:
         time_away = (time + goal_minute) % dep
@@ -102,65 +116,107 @@ def quick_correct(time):
     return True
 
 
+Z = list(zip(departs, offsets3))
+eqn = "solve for x, "
+# eqn = ""
+for dep, offset in Z:
+    eqn += f"x = {dep - offset} mod {dep}, "
+print(eqn)  # me trying to make a wolfram alpha query post
+
+from functools import reduce
+
+# https://rosettacode.org/wiki/Chinese_remainder_theorem#Python_3.6
+def chinese_remainder(n, a):
+    sum = 0
+    prod = reduce(lambda a, b: a * b, n)
+    for n_i, a_i in zip(n, a):
+        p = prod // n_i
+        sum += a_i * mul_inv(p, n_i) * p
+    return sum % prod
+
+
+def mul_inv(a, b):
+    b0 = b
+    x0, x1 = 0, 1
+    if b == 1:
+        return 1
+    while a > 1:
+        q = a // b
+        a, b = b, a % b
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0:
+        x1 += b0
+    return x1
+
+
+n = departs[:]
+a = offsets3[:]
+# print("1068781, correct small_input")
+cr = chinese_remainder(n, a)
+ans(cr)  # 1012171816131114
+exit()
+
+"""
+wrong attempts below
+"""
 #      703
-a = [0, 1, 2, 3, 4, 5, 6, 7, 8]
-print(departs)
-print(allmod(first, departs))
 
-marked = dict()
-diffs = dict()
-for i in range(len(departs)):
-    marked[i] = False
-while True:
-    # CT = correct_time(idx, departs)
-    # for i, v in enumerate(CT):
-    #     if v:
-    #         if not marked[i]:
-    #             print(f"{i} depart({departs[i]}) every {idx}")
-    #             diffs[i] = idx
-    #         marked[i] = True
-    # if all(marked.values()):
-    #     break
-    # if AM[2] == 2:
-    #     print(idx - l)
-    #     l = idx
-    if quick_correct(idx):
-        ans(idx)
-        exit()
-        break
-    # if all(correct_time(idx, departs)):
-    #     ans(idx)
-    #     break
-    idx += first
-print(diffs.values())
+# a = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+# print(departs)
+# print(allmod(first, departs))
 
-from math import gcd  # Python versions 3.5 and above
+# marked = dict()
+# diffs = dict()
+# for i in range(len(departs)):
+#     marked[i] = False
+# while True:
+#     # CT = correct_time(idx, departs)
+#     # for i, v in enumerate(CT):
+#     #     if v:
+#     #         if not marked[i]:
+#     #             print(f"{i} depart({departs[i]}) every {idx}")
+#     #             diffs[i] = idx
+#     #         marked[i] = True
+#     # if all(marked.values()):
+#     #     break
+#     # if AM[2] == 2:
+#     #     print(idx - l)
+#     #     l = idx
+#     if quick_correct(idx):
+#         ans(idx)  # this will theoretically finish
+#         exit()
+#         break
+#     # if all(correct_time(idx, departs)):
+#     #     ans(idx)
+#     #     break
+#     idx += first
+# print(diffs.values())
 
-# from fractions import gcd # Python versions below 3.5
-from functools import reduce  # Python version 3.x
-
-
-def lcm(denominators):
-    return reduce(lambda a, b: a * b // gcd(a, b), denominators)
+# # from fractions import gcd # Python versions below 3.5
+# from functools import reduce  # Python version 3.x
 
 
-dv = list(diffs.values())
-GOAL_SMALL = 1068781
-l = lcm(diffs.values())
-s = 1
-for i in diffs.values():
-    s *= i
-    print(GOAL_SMALL / i)
-print("multiplied: ", s)
-print(correct_time(s, departs))
-
-print("lcm: ", l)
-print(correct_time(l, departs))
+# def lcm(denominators):
+#     return reduce(lambda a, b: a * b // gcd(a, b), denominators)
 
 
-print("gcd: ", gcd(*dv))
-print(correct_time(l, departs))
+# dv = list(diffs.values())
+# GOAL_SMALL = 1068781
+# l = lcm(diffs.values())
+# s = 1
+# for i in diffs.values():
+#     s *= i
+#     print(GOAL_SMALL / i)
+# print("multiplied: ", s)
+# print(correct_time(s, departs))
+
+# print("lcm: ", l)
+# print(correct_time(l, departs))
 
 
-print("all true below? (1068781)")
-print(correct_time(1068781, departs))
+# print("gcd: ", gcd(*dv))
+# print(correct_time(l, departs))
+
+
+# print("all true below? (1068781)")
+# print(correct_time(1068781, departs))
