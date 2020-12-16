c111ec7260b0e23b2c348c117a6ef5b19236cc05
kleutzinger <6435727+kleutzinger@users.noreply.github.com>
2020-12-16 17:06:58 -0500

    run before midnight, gremlins style

--- new_day.fish
+++ new_day.fish
@@ -3,10 +3,10 @@
 # abbreviated to `advent` (run from any directory)
 set advent_dir /home/kevin/gits/advent-of-code-2020
 cd $advent_dir
-set -x TZ "America/New_York" # -x sets environment variables
-set day (date +%e) # 2 digit day of month 
+# tomorrow's date (run this before midnight PST)
+set day ( date --date="tomorrow" +%e )
 if test -d $day
-    echo "folder $day exists."
+    echo "folder $day exists. did not overwrite"
     echo "cd $day"
     cd $day
     cdv . # activate virtualenv
@@ -14,6 +14,7 @@ if test -d $day
 end
 mkdir $day
 cp boilerplate.py $day/run.py
+echo "cd to new directory $day"
 cd $day
 cdv . # activate virtualenv
 touch input.txt
